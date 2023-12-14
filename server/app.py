2d00d6b7327fb93c6d858d0c7eb42833e80c8730
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-12-14 18:52:11 +0300

    Complete HATEOAS

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -15,7 +15,28 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+ma = Marshmallow(app)
 api = Api(app)
+
+class NewsletterSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Newsletter
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
 
 class Index(Resource):
 
@@ -38,10 +59,10 @@ class Newsletters(Resource):
 
     def get(self):
         
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        response_dict = Newsletter.query.all()
 
         response = make_response(
-            response_dict_list,
+            newsletters_schema.dump(response_dict),
             200,
         )
 
@@ -57,7 +78,7 @@ class Newsletters(Resource):
         db.session.add(new_record)
         db.session.commit()
 
-        response_dict = new_record.to_dict()
+        response_dict = newsletter_schema.dump(new_record)
 
         response = make_response(
             response_dict,
@@ -72,10 +93,10 @@ class NewsletterByID(Resource):
 
     def get(self, id):
 
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+        response_dict = Newsletter.query.filter_by(id=id).first()
 
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(response_dict),
             200,
         )
 
@@ -90,7 +111,7 @@ class NewsletterByID(Resource):
         db.session.add(record)
         db.session.commit()
 
-        response_dict = record.to_dict()
+        response_dict = newsletter_schema.dump(record)
 
         response = make_response(
             response_dict,
